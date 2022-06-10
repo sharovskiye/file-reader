cf4cee617b08a65c4f226bbc85040822bfbca541
Yevhenii <sharovskiyevgeniy@gmail.com>
2022-06-10 22:40:58 +0300

    feat: complete a test task

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import { ChangeEvent, useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [countValidPassword, setCountValidPassword] = useState(0);
+
+  const getCountValidPassword = (lines: string[]) => {
+    return lines.reduce((acc, line) => {
+      const [condition, password] = line.split(': ');
+      const [letter, count] = condition.split(' ');
+      const [minCount, maxCount] = count.split('-');
+      const reg = new RegExp(letter, 'g');
+
+      const countLetters = password.match(reg)?.length || 0;
+
+      return countLetters >= Number(minCount) &&
+        countLetters <= Number(maxCount)
+        ? acc + 1
+        : acc;
+    }, 0);
+  };
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files?.[0]) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const result = e?.target?.result as string;
+        setCountValidPassword(getCountValidPassword(result.split('\r\n')));
+      };
+
+      reader.readAsText(file);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <main className="App-main">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Count validation password: {countValidPassword}</p>
+        <input type="file" onChange={onChange} accept=".txt" />
+      </main>
     </div>
   );
 }
